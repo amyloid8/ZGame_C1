3f19b51d2633bddeac2f1ca45c68f1612fd6bbec
Matt Song <matzsong@mit.edu>
2020-07-19 22:42:45 -0400

    Changed NPC_STATES from enums to dictionaries

--- gym_zgame/envs/enums/NPC_STATES.py
+++ gym_zgame/envs/enums/NPC_STATES.py
@@ -1,125 +1,88 @@
-from enum import IntEnum
 import random
 import warnings
 
 
-class NPC_STATES_DEAD(IntEnum):
-    ALIVE = 0
-    DEAD = 1
-    ASHEN = 2
+class NPC_STATES_DEAD:
+    dead_state_Dict = {'ALIVE':0,'DEAD':1,'ASHEN':2}
 
     @staticmethod
     def print():
-        for state in NPC_STATES_DEAD:
-            print('{0} -> {1}'.format(state.value, state.name))
+        for key in dead_state_Dict:
+            print('{0} -> {1}'.format(key, dead_state_Dict[key]))
 
     @classmethod
     def get_random(cls):
-        return random.choice(list(NPC_STATES_DEAD))
+        return random.choice(dead_state_Dict.keys())
 
     @staticmethod
     def get_value_from_string(state):
-        if state.upper() == 'ALIVE':
-            return NPC_STATES_DEAD.ALIVE.value
-        elif state.upper() == 'DEAD':
-            return NPC_STATES_DEAD.DEAD.value
-        elif state.upper() == 'ASHEN':
-            return NPC_STATES_DEAD.ASHEN.value
+        if state.upper() in dead_state_Dict:
+            return dead_state_Dict[state.upper()]
         else:
-            warnings.warn('Tried to convert string ({}) to NPC_STATES_DEAD enum and failed; returned ALIVE'.format(state))
-            return NPC_STATES_DEAD.ALIVE.value
+            warnings.warn('String ({}) not in dead_state_Dict; returned ALIVE'.format(state))
+            return dead_state_Dict['ALIVE']
 
     @staticmethod
     def get_name_from_string(state):
-        if state.upper() == 'ALIVE':
-            return NPC_STATES_DEAD.ALIVE.name
-        elif state.upper() == 'DEAD':
-            return NPC_STATES_DEAD.DEAD.name
-        elif state.upper() == 'ASHEN':
-            return NPC_STATES_DEAD.ASHEN.name
+        if state.upper() in dead_state_Dict:
+            return state.upper()
         else:
-            warnings.warn('Tried to convert string ({}) to NPC_STATES_DEAD enum and failed; returned ALIVE'.format(state))
-            return NPC_STATES_DEAD.ALIVE.name
+            warnings.warn('String ({}) not in dead_state_Dict; returned ALIVE'.format(state))
+            return 'ALIVE'
 
 
-class NPC_STATES_ZOMBIE(IntEnum):
-    HUMAN = 0
-    ZOMBIE_BITTEN = 1
-    ZOMBIE = 2
+class NPC_STATES_ZOMBIE:
+    zombie_state_Dict = {'HUMAN':0,'ZOMBIE_BITTEN':1,'ZOMBIE':2}
 
     @staticmethod
     def print():
-        for state in NPC_STATES_ZOMBIE:
-            print('{0} -> {1}'.format(state.value, state.name))
+        for key in zombie_state_Dict:
+            print('{0} -> {1}'.format(key, zombie_state_Dict[key]))
 
     @classmethod
     def get_random(cls):
-        return random.choice(list(NPC_STATES_ZOMBIE))
+        return random.choice(zombie_state_Dict.keys())
 
     @staticmethod
     def get_value_from_string(state):
-        if state.upper() == 'HUMAN':
-            return NPC_STATES_ZOMBIE.HUMAN.value
-        elif state.upper() == 'ZOMBIE_BITTEN':
-            return NPC_STATES_ZOMBIE.ZOMBIE_BITTEN.value
-        elif state.upper() == 'ZOMBIE':
-            return NPC_STATES_ZOMBIE.ZOMBIE.value
+        if state.upper() in zombie_state_Dict:
+            return zombie_state_Dict[state.upper()]
         else:
-            warnings.warn('Tried to convert string ({}) to NPC_STATES_ZOMBIE enum and failed; returned HUMAN'.format(state))
-            return NPC_STATES_ZOMBIE.HUMAN.value
-
+            warnings.warn('String ({}) not in zombie_state_Dict; returned HUMAN'.format(state))
+            return 'HUMAN'
     @staticmethod
     def get_name_from_string(state):
-        if state.upper() == 'HUMAN':
-            return NPC_STATES_ZOMBIE.HUMAN.name
-        elif state.upper() == 'ZOMBIE_BITTEN':
-            return NPC_STATES_ZOMBIE.ZOMBIE_BITTEN.name
-        elif state.upper() == 'ZOMBIE':
-            return NPC_STATES_ZOMBIE.ZOMBIE.name
+        if state.upper() in zombie_state_Dict:
+            return state.upper()
         else:
-            warnings.warn('Tried to convert string ({}) to NPC_STATES_ZOMBIE enum and failed; returned HUMAN'.format(state))
-            return NPC_STATES_ZOMBIE.HUMAN.name
+            warnings.warn('String ({}) not in zombie_state_Dict; returned HUMAN'.format(state))
+            return 'HUMAN'
 
 
-class NPC_STATES_FLU(IntEnum):
-    HEALTHY = 0
-    INCUBATING = 1
-    FLU = 2
-    IMMUNE = 3
+class NPC_STATES_FLU:
+    flu_state_Dict = {'HEALTHY':0,'INCUBATING':1,'FLU':2,'IMMUNE':3}
 
     @staticmethod
     def print():
-        for state in NPC_STATES_FLU:
-            print('{0} -> {1}'.format(state.value, state.name))
+        for key in flu_state_Dict:
+            print('{0} -> {1}'.format(key, flu_state_Dict[key]))
 
     @classmethod
     def get_random(cls):
-        return random.choice(list(NPC_STATES_FLU))
+        return random.choice(flu_state_Dict.keys())
 
     @staticmethod
     def get_value_from_string(state):
-        if state.upper() == 'HEALTHY':
-            return NPC_STATES_FLU.HEALTHY.value
-        elif state.upper() == 'INCUBATING':
-            return NPC_STATES_FLU.INCUBATING.value
-        elif state.upper() == 'FLU':
-            return NPC_STATES_FLU.FLU.value
-        elif state.upper() == 'IMMUNE':
-            return NPC_STATES_FLU.IMMUNE.value
+        if state.upper() in flu_state_Dict:
+            return flu_state_Dict[state.upper()]
         else:
-            warnings.warn('Tried to convert string ({}) to NPC_STATES_FLU enum and failed; returned HEALTHY'.format(state))
-            return NPC_STATES_FLU.HEALTHY.value
+            warnings.warn('String ({}) not in flu_state_Dict; returned HEALTHY'.format(state))
+            return flu_state_Dict['HEALTHY']
 
     @staticmethod
     def get_name_from_string(state):
-        if state.upper() == 'HEALTHY':
-            return NPC_STATES_FLU.HEALTHY.name
-        elif state.upper() == 'INCUBATING':
-            return NPC_STATES_FLU.INCUBATING.name
-        elif state.upper() == 'FLU':
-            return NPC_STATES_FLU.FLU.name
-        elif state.upper() == 'IMMUNE':
-            return NPC_STATES_FLU.IMMUNE.name
+        if state.upper() in flu_state_Dict:
+            return state.upper()
         else:
-            warnings.warn('Tried to convert string ({}) to NPC_STATES_FLU enum and failed; returned HEALTHY'.format(state))
-            return NPC_STATES_FLU.HEALTHY.name
+            warnings.warn('String ({}) not in flu_state_Dict; returned HEALTHY'.format(state))
+            return 'HEALTHY'
