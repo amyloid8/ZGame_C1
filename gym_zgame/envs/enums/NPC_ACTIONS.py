ee6a9c042d1a4006c567288ee4aacc0bddf5e5b5
Matt Song <matzsong@mit.edu>
2020-07-19 22:08:36 -0400

    NPC_ACTIONS changed from enums to dictionary

--- gym_zgame/envs/enums/NPC_ACTIONS.py
+++ gym_zgame/envs/enums/NPC_ACTIONS.py
@@ -1,20 +1,15 @@
-from enum import IntEnum
 import random
 import warnings
 from gym_zgame.envs.enums.PLAYER_ACTIONS import LOCATIONS
 
 
-class NPC_ACTIONS(IntEnum):
-    STAY = 0
-    N = 1
-    S = 2
-    E = 3
-    W = 4
+class NPC_ACTIONS():
+    action_Dict = {'STAY': 0, 'N': 1,'S':2,"E":3,"W":4}
 
     @staticmethod
     def print():
-        for action in NPC_ACTIONS:
-            print('{0} -> {1}'.format(action.value, action.name))
+        for key in action_Dict:
+            print('{0} -> {1}'.format(key, action_Dict[key]))
 
     @classmethod
     def get_random(cls):
@@ -22,45 +17,29 @@ class NPC_ACTIONS(IntEnum):
 
     @staticmethod
     def get_value_from_string(action):
-        if action.upper() == 'STAY':
-            return NPC_ACTIONS.STAY.value
-        elif action.upper() == 'N':
-            return NPC_ACTIONS.N.value
-        elif action.upper() == 'S':
-            return NPC_ACTIONS.S.value
-        elif action.upper() == 'E':
-            return NPC_ACTIONS.E.value
-        elif action.upper() == 'W':
-            return NPC_ACTIONS.W.value
+        if action.upper() in action_Dict:
+            return action_Dict[action]
         else:
             warnings.warn('Tried to convert string ({}) to NPC_ACTIONS enum and failed; returned STAY'.format(action))
-            return NPC_ACTIONS.STAY.value
+            return action_Dict['STAY']
 
     @staticmethod
     def get_name_from_string(action):
-        if action.upper() == 'STAY':
-            return NPC_ACTIONS.STAY.name
-        elif action.upper() == 'N':
-            return NPC_ACTIONS.N.name
-        elif action.upper() == 'S':
-            return NPC_ACTIONS.S.name
-        elif action.upper() == 'E':
-            return NPC_ACTIONS.E.name
-        elif action.upper() == 'W':
-            return NPC_ACTIONS.W.name
+        if action.upper() in action_Dict:
+            return action.upper()
         else:
             warnings.warn('Tried to convert string ({}) to NPC_ACTIONS enum and failed; returned STAY'.format(action))
-            return NPC_ACTIONS.STAY.name
+            return 'STAY'
 
     @staticmethod
     def reverse_action(npc_action):
-        if npc_action is NPC_ACTIONS.N:
-            return NPC_ACTIONS.S
-        if npc_action is NPC_ACTIONS.S:
-            return NPC_ACTIONS.N
-        if npc_action is NPC_ACTIONS.E:
-            return NPC_ACTIONS.W
-        if npc_action is NPC_ACTIONS.W:
-            return NPC_ACTIONS.E
-        if npc_action is NPC_ACTIONS.STAY:
-            return NPC_ACTIONS.STAY
+        if npc_action is 'N':
+            return 'S'
+        if npc_action is 'S':
+            return 'N'
+        if npc_action is 'E':
+            return 'W'
+        if npc_action is 'W':
+            return 'E'
+        if npc_action is 'STAY':
+            return 'STAY'
